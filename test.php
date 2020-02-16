3a0249326e07b3bc54a574e17e459784e287d76d
veeresh <drveresh@gmail.com>
2020-02-16 13:02:50 +0530

    adding initial test files

---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>PHP Test Page</h1>
+
+<?php
+echo "<br>Hello World - Php Test Success!";
+echo "<br>REMOTE_ADDR: ".$_SERVER["REMOTE_ADDR"];
+echo "<br>HTTP_HOST: ".$_SERVER["HTTP_HOST"];
+?>
+
+</body>
+</html>
